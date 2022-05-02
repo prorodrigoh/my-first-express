8b2ac691154ab11a1dddbdaadb9dd4ec0d331c7a
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-05-02 18:51:54 -0400

    Added comments on code

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const PORT = 3030
 const app = express()
 app.use(express.json())
 
-app.get('/', welcomeMessage)
+app.get('/', welcomeMessage)    // function in a different file path
 
-app.get('/hello', hello)
+app.get('/hello', hello)    // simple GET request
 
-app.get('/hello/:person', helloPerson)
+app.get('/hello/:person', helloPerson)  // pass a parameter within the GET method
 
-app.post('/hello', greetNewPerson)
+app.post('/hello', greetNewPerson)  // POST method - uses Postman to send the POST with the local host and interact with teh app
 
 app.listen(PORT, () => console.log(`Listening to http://localhost:${PORT}...`))
